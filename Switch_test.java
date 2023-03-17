d8a947ce7274eb11db420cdd3dd470d3c31f29af
Siddheshwar Fulari <127471394+Siddheshwarfulari@users.noreply.github.com>
2023-03-17 14:13:00 +0530

    Add files via upload

---
+++ Switch_test.java
@@ -0,0 +1,16 @@
+class Switch_test {
+public static void main (String []args){
+int a = 10;
+
+switch(a){
+case 0:
+System.out.println("Number is 0");
+break;
+case 10:
+System.out.println("Number is perfect");
+break;
+default:
+System.out.println(a);
+}
+}
+}
---
+++ ifelseladder.java
@@ -0,0 +1,19 @@
+ class ifelseladder{
+
+ public static void main(String[] args){
+ String name= "Siddhu";
+
+if(name == "sankath") {
+  System.out.println("name is sankath");
+  }
+else if (name == "Ramu"){
+  System.out.println("name is Ramu");
+  }
+else if (name == "Gopi"){
+  }
+
+else {
+   System.out.println("name");
+  }
+  }
+  }
